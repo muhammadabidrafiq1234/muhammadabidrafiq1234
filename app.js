27fd709e20881c2bca3a223f0e11403f3ea31ece
muhammadabidrafiq1234 <decabidrafiq2008@gmail.com>
2024-10-08 20:42:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+//var arr =["saad","osama","taha","bilal",]
+//document.write(arr.indexOf(taha))
+//document.write(arr.findIndex("saad"))
+
+//var naam =red
+//document.write(naam/arr perc=72.9
+//document.write(Math.floor(perc))
+
+
+

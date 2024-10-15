3596607ddc4db325f9cb48071b7894a47f01d317
muhammadabidrafiq1234 <decabidrafiq2008@gmail.com>
2024-10-15 20:49:19 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
-//var arr =["saad","osama","taha","bilal",]
-//document.write(arr.indexOf(taha))
-//document.write(arr.findIndex("saad"))
+//var date = new Date()
+//var convert = date.toString()
+//var copy = convert.slice(16,25)
+//document.write(convert)
 
-//var naam =red
-//document.write(naam/arr perc=72.9
-//document.write(Math.floor(perc))
+//var date = new Date()
+//var getday = date.getFullYear()
+//document.write(getday)
+
+//var date = new Date()
+//var datemili = date.getTime
+//document.write(datemilisecond)
+
+//var date = new Date()
+//date.setFullYear(2001)
+
+
+//var now = new Date
+//var thehr = now.getHours
+//var themin = now.getMinutes
+//alert("current time:" + thehr + ":" + themin)
+
+
+//function greeuser () {
+   // alert("hello these")
+//}
+    
+
+//var thesum
+//addnumber()
+//functionaddnumber ()  {
+//thesum = 2 + 2
+
+//}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

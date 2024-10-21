bfd833775dceed83c6e526be85b467575e127918
muhammadabidrafiq1234 <decabidrafiq2008@gmail.com>
2024-10-21 20:03:02 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-var originalnew =23;
-var newnum = originalnum +7;
+//var originalnew =23;
+//var newnum = originalnum +7;
 
-var originalnum =23;
-var numtobeadded =7;
-var newnum =originalnum + numtobeadded;
+//var originalnum =23;
+//var numtobeadded =7;
+//var newnum =originalnum + numtobeadded;
 
-var originalnum = 90;
-originalnum = originalnum +10;
+//var originalnum = 90;
+//originalnum = originalnum +10;
 
-var originalnum ="23";
-var newnum =originalnum +7;
+//var originalnum ="23";
+//var newnum =originalnum +7;
 
-var originalnum = 23;
-var newnum = originalnum +7;
+//var originalnum = 23;
+//var newnum = originalnum +7;
 
 
+var popularnum =2+2;
+alert(2+2);
+var popularnum = 12-24;
+var popularnum = 3*12;
+var num =10;
+var anothernum =1;
+var popularnum = num+anothernum;
 
 
 

2c305b6c693038e824f3b8b45f5648bb56ca787e
muhammadabidrafiq1234 <decabidrafiq2008@gmail.com>
2024-10-21 20:31:54 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -15,17 +15,36 @@
 //var newnum = originalnum +7;
 
 
-var popularnum =2+2;
-alert(2+2);
-var popularnum = 12-24;
-var popularnum = 3*12;
-var num =10;
-var anothernum =1;
-var popularnum = num+anothernum;
+//var popularnum =2+2;
+//alert(2+2);
+//var popularnum = 12-24;
+//var popularnum = 3*12;
+//var num =10;
+//var anothernum =1;
+//var popularnum = num+anothernum;
 
+var x =prompt("where does the pope live");
+if (x ==="vatican"){
+alert("correct")
 
+}
 
+var correctanswer ="vatican";
+if (x==="correct answer"){
+alert("correct");
 
+}
+
+//conditional  statement
+const value = 2>1;
+//console.log(type of value);
+const value2 = 2>1;
+if (value2){
+console.log ("hello world");
+
+}else{
+    console.log("hello world");
+}
 
 
 

365ca318a893f5039e19cf048ae038c278fd91d5
muhammadabidrafiq1234 <decabidrafiq2008@gmail.com>
2024-10-22 19:21:57 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -23,28 +23,52 @@
 //var anothernum =1;
 //var popularnum = num+anothernum;
 
-var x =prompt("where does the pope live");
-if (x ==="vatican"){
-alert("correct")
+//var x =prompt("where does the pope live");
+//if (x ==="vatican"){
+//alert("correct")
 
-}
+//}
 
-var correctanswer ="vatican";
-if (x==="correct answer"){
-alert("correct");
+//var correctanswer ="vatican";
+//if (x==="correct answer"){
+//alert("correct");
 
-}
+//}
 
 //conditional  statement
-const value = 2>1;
+//const value = 2>1;
 //console.log(type of value);
-const value2 = 2>1;
-if (value2){
-console.log ("hello world");
+//const value2 = 2>1;
+//if (value2){
+//console.log ("hello world");
 
-}else{
-    console.log("hello world");
+//}else{
+    //console.log("hello world");
+
+//}
+
+const num1 = 6;
+const num2 = 6;
+const result = num1 >= num2;
+const value =false;
+if (value){
+    console.log("value is true")
 }
+
+
+const name = "peter";
+const age = "24";
+if (name ==="bob" || age===24);{
+console.log("hello these user");
+
+}    {
+    console.log("wrong value");
+}
+
+
+
+
+
 
 
 
